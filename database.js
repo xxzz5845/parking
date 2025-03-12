d801b84ddd73c12a5b9137eeb4aad8dc87dc44d7
xxzz5845 <84029285+xxzz5845@users.noreply.github.com>
2025-03-12 11:01:44 +0800

    Add files via upload

---
+++ database.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDDbQLPpKYlLlXj7-q5QY3fvvhMRj6Ca08",
+    authDomain: "sa-database-fff6b.firebaseapp.com",
+    projectId: "sa-database-fff6b",
+    storageBucket: "sa-database-fff6b.appspot.com",
+    messagingSenderId: "892692361698",
+    appId: "1:892692361698:web:69b5f272fff0af4525c9bb",
+    measurementId: "G-2T10MZ1ZS6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
